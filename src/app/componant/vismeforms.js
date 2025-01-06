4c982ae1208adae350cbb87a5dd10ded3c44e9d6
RAMADAN-MAHDY <104697578+RAMADAN-MAHDY@users.noreply.github.com>
2025-01-06 05:31:29 +0200

    update

--- src/app/componant/vismeforms.js
+++ src/app/componant/vismeforms.js
@@ -6,7 +6,7 @@ const Vismeforms =() =>{
 
 return(
     <>
-   <main className="relative mt-[0px] h-[20%] sm:h-[30%]  w-[100%] sm:w-[100%] sm-mt-3 bg-[#121431] z-10">
+   <section className="relative mt-[0px] h-[20%] sm:h-[30%]  w-[100%] sm:w-[100%] sm-mt-3 bg-[#121431] z-10">
 {/* SpiderWeb as background */}
  <div className="absolute top-0 left-0 w-full h-full -z-10">
     <SpiderWeb height="100%" />
@@ -15,7 +15,7 @@ return(
 <script src="https://static-bundles.visme.co/forms/vismeforms-embed.js" async></script>
 <div className="bg-[#0c0c0c93] w-full h-6 sm:mt-[-90px] mt-[-120px]"></div>
 <div className="bg-[#fa070700] w-full h-full top-0 left-0 z-50 absolute"></div>
-</main>
+</section>
     
  </>
 )
