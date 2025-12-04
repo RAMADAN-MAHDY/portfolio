1d4c4d9dce6ef3e287aaed99f7f7306a83f391af
RAMADAN-MAHDY <104697578+RAMADAN-MAHDY@users.noreply.github.com>
2025-12-04 15:08:47 +0200

    chore: update profile images and favicon reference
    - Replace old profile image with new version (ramadan-mahdy-fullstack-developer3.jpg)
    - Remove unused image file (451548470_3829658120605216_1402526888296984978_n.jpg)
    - Update favicon reference in layout to use new profile image

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -36,7 +36,7 @@ function RootLayout({ children }: LocaleLayoutProps) {
             <head>
                 <meta name="google" content="notranslate" />
                 <meta name="google-site-verification" content="-1HDfMA8r2MPfNp6oa5PTR1Pe2Z-g4CThr_hEn1rIsM" />
-                <link rel="icon" href="/451548470_3829658120605216_14025268882969849787_n.JPG" sizes="32x32" />
+                <link rel="icon" href="/ramadan-mahdy-fullstack-developer3.jpg" sizes="32x32" />
                 <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-1900509020605535"
                     crossOrigin="anonymous"></script>
                 <script
